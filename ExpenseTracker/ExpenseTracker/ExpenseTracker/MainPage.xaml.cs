64ac7887bddca4dc1e492142cecebce8d0a9fde0
Steffen-Jason <ste15062@byui.edu>
2019-09-30 19:43:10 -0600

    test

--- ExpenseTracker/ExpenseTracker/ExpenseTracker/MainPage.xaml.cs
+++ ExpenseTracker/ExpenseTracker/ExpenseTracker/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
+//Testing to make sure I can update the repository. -JS
+
 namespace ExpenseTracker
 {
    public partial class MainPage : ContentPage
