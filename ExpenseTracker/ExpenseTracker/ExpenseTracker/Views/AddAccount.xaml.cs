f96671d8fbc13914d2c249189d26a3c4ae1d7b64
Steffen-Jason <ste15062@byui.edu>
2019-11-17 10:55:51 -0700

    adding addaccount page and temp buttons to login page

---
+++ ExpenseTracker/ExpenseTracker/ExpenseTracker/Views/AddAccount.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExpenseTracker
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddAccount : ContentPage
+    {
+        public AddAccount()
+        {
+            InitializeComponent();
+        }
+
+        async void OnSaveAccountButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+
+        async void OnDeleteAccountButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
