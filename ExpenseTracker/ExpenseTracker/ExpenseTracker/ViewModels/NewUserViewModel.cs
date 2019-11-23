118d7ea58102f197759720ba3d404f5cbaf006e1
Steffen-Jason <ste15062@byui.edu>
2019-11-23 14:15:43 -0700

    Adding newuser viewmodel and database call to newuser page

---
+++ ExpenseTracker/ExpenseTracker/ExpenseTracker/ViewModels/NewUserViewModel.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExpenseTracker.Model;
+using System.Security.Cryptography;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace ExpenseTracker.ViewModels
+{
+    class NewUserViewModel : INotifyPropertyChanged
+    {
+        public DataQuery_Mod DataQuery;
+        private bool _IsBusy = false;
+        public bool IsBusy { get { return _IsBusy; } set { _IsBusy = value; OnPropertyChanged(nameof(IsBusy)); } }
+        public int User_ID { get; set; }
+        private ObservableCollection<Users> _UsersInfo;
+        public ObservableCollection<Users> UsersInfo
+        {
+            get { return _UsersInfo; }
+            set
+            {
+                _UsersInfo = value;
+                OnPropertyChanged(nameof(UsersInfo));
+            }
+        }
+
+        private string _Firstname;
+        public string Firstname
+        {
+            get { return _Firstname; }
+            set
+            {
+                _Firstname = value;
+
+                OnPropertyChanged(nameof(Firstname));
+            }
+        }
+
+        private string _Lastname;
+        public string Lastname
+        {
+            get { return _Lastname; }
+            set
+            {
+                _Lastname = value;
+
+                OnPropertyChanged(nameof(Lastname));
+            }
+        }
+
+        private string _Username;
+        public string Username
+        {
+            get { return _Username; }
+            set
+            {
+                _Username = value;
+
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+
+        private string _FirstPassword;
+        public string FirstPasswordHash;
+        public string FirstPassword
+        {
+            get { return _FirstPassword; }
+            set
+            {
+                _FirstPassword = value;
+                using (MD5 md5Hash = MD5.Create())
+                {
+                    FirstPasswordHash = HashClass.GetMd5Hash(md5Hash, value);
+                }
+                OnPropertyChanged(nameof(FirstPassword));
+            }
+        }
+
+        private string _SecondPassword;
+        public string SecondPasswordHash;
+        public string SecondPassword
+        {
+            get { return _SecondPassword; }
+            set
+            {
+                _SecondPassword = value;
+                using (MD5 md5Hash = MD5.Create())
+                {
+                    SecondPasswordHash = HashClass.GetMd5Hash(md5Hash, value);
+                }
+                OnPropertyChanged(nameof(SecondPassword));
+            }
+        }
+
+        public NewUserViewModel()
+        {
+            DataQuery = new DataQuery_Mod();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string name = "")
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+    }
+}
--- ExpenseTracker/ExpenseTracker/ExpenseTracker/Views/NewUser.xaml.cs
+++ ExpenseTracker/ExpenseTracker/ExpenseTracker/Views/NewUser.xaml.cs
@@ -5,21 +5,136 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using ExpenseTracker.ViewModels;
 
 namespace ExpenseTracker
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class NewUser : ContentPage
     {
+        NewUserViewModel viewModel;
+        
         public NewUser()
         {
             InitializeComponent();
+            BindingContext = viewModel = new NewUserViewModel();
+        }
+        //Need to add to XAML File <local:RequiredValidatorBehavior x:Name="passwordValidator"/>
+        public class RequiredValidatorBehavior : Behavior<Entry>
+        {
+            static readonly BindablePropertyKey IsValidPropertyKey = BindableProperty.CreateReadOnly("IsValid", typeof(bool), typeof(RequiredValidatorBehavior), false);
+            static readonly BindableProperty IsValidProperty = IsValidPropertyKey.BindableProperty;
+
+            public bool IsValid
+            {
+                get { return (bool)base.GetValue(IsValidProperty); }
+                private set { base.SetValue(IsValidPropertyKey, value); }
+            }
+
+            protected override void OnAttachedTo(Entry bindable)
+            {
+                bindable.Unfocused += HandleFocusChanged;
+                base.OnAttachedTo(bindable);
+            }
+            protected override void OnDetachingFrom(Entry bindable)
+            {
+                bindable.Unfocused -= HandleFocusChanged;
+                base.OnDetachingFrom(bindable);
+            }
+            void HandleFocusChanged(object sender, FocusEventArgs e)
+            {
+                IsValid = !string.IsNullOrEmpty(((Entry)sender).Text);
+            }
+        }
+
+        //Need to add to XAML file: <local:ConfirmPasswordBehavior x:Name="confirmPasswordBehavior" CompareToEntry="{Binding Source={x:Reference password}}" />
+        public class ComparisonBehavior : Behavior<Entry>
+        {
+            private Entry thisEntry;
+
+            static readonly BindablePropertyKey IsValidPropertyKey = BindableProperty.CreateReadOnly("IsValid", typeof(bool), typeof(ComparisonBehavior), false);
+            public static readonly BindableProperty IsValidProperty = IsValidPropertyKey.BindableProperty;
+
+            public static readonly BindableProperty CompareToEntryProperty = BindableProperty.Create("CompareToEntry", typeof(Entry), typeof(ComparisonBehavior), null);
+
+            public Entry CompareToEntry
+            {
+                get { return (Entry)base.GetValue(CompareToEntryProperty); }
+                set
+                {
+                    base.SetValue(CompareToEntryProperty, value);
+                    if (CompareToEntry != null)
+                        CompareToEntry.TextChanged -= baseValue_changed;
+                    value.TextChanged += baseValue_changed;
+                }
+            }
+
+            void baseValue_changed(object sender, TextChangedEventArgs e)
+            {
+                IsValid = ((Entry)sender).Text.Equals(thisEntry.Text);
+                thisEntry.TextColor = IsValid ? Color.Default : Color.Red;
+            }
+
+
+            public bool IsValid
+            {
+                get { return (bool)base.GetValue(IsValidProperty); }
+                private set { base.SetValue(IsValidPropertyKey, value); }
+            }
+            protected override void OnAttachedTo(Entry bindable)
+            {
+                thisEntry = bindable;
+
+                if (CompareToEntry != null)
+                    CompareToEntry.TextChanged += baseValue_changed;
+
+                bindable.TextChanged += HandleTextChanged;
+                base.OnAttachedTo(bindable);
+            }
+
+            protected override void OnDetachingFrom(Entry bindable)
+            {
+                bindable.TextChanged -= HandleTextChanged;
+                if (CompareToEntry != null)
+                    CompareToEntry.TextChanged -= baseValue_changed;
+                base.OnDetachingFrom(bindable);
+            }
+
+            void HandleTextChanged(object sender, TextChangedEventArgs e)
+            {
+                string theBase = CompareToEntry.Text;
+                string confirmation = e.NewTextValue;
+                IsValid = (bool)theBase?.Equals(confirmation);
+
+                ((Entry)sender).TextColor = IsValid ? Color.Green : Color.Red;
+            }
         }
 
         async void OnSubmitButtonClicked(object sender, EventArgs e)
         {
+            DateTime myDateTime = DateTime.Now;
+            string sqlFormattedDate = myDateTime.ToString("yyyy-MM-dd");
 
-            await Navigation.PopAsync();
+            try
+            {
+                viewModel.IsBusy = true;
+                viewModel.DataQuery.expenseSelect = "INSERT INTO users VALUES ";
+                viewModel.DataQuery.expenseWhere = "('" + viewModel.Username + "', '" + viewModel.Firstname + "', '" + viewModel.Lastname + "', '" + viewModel.SecondPasswordHash + "', '" + sqlFormattedDate + "')";
+                if (viewModel.FirstPasswordHash == viewModel.SecondPasswordHash)
+                {
+                  viewModel.UsersInfo = viewModel.DataQuery.ExecuteAQuery<Users>();
+                }
+
+                
+                
+                viewModel.IsBusy = false;
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Unsuccessful user creation", ex.Message, "OK");
+                viewModel.IsBusy = false;
+            }
         }
 
         async void OnCancelButtonClicked(object sender, EventArgs e)
