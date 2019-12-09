02acd4047c9ff2661a54e81a96886299f5602653
Steffen-Jason <ste15062@byui.edu>
2019-12-08 18:47:08 -0700

    Adding placeholders for SQL query

--- ExpenseTracker/ExpenseTracker/ExpenseTracker/Views/AddItem.xaml.cs
+++ ExpenseTracker/ExpenseTracker/ExpenseTracker/Views/AddItem.xaml.cs
@@ -6,15 +6,20 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using ExpenseTracker.ViewModels;
 
 namespace ExpenseTracker
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AddItem : ContentPage
     {
+        AddItemViewModel viewModel;
         public AddItem()
         {
             InitializeComponent();
+            BindingContext = viewModel = new AddItemViewModel();
+            viewModel.TransType = "Test";
+            viewModel.Account = "Test";
         }
         async void OnSaveButtonClicked(object sender, EventArgs e)
         {
